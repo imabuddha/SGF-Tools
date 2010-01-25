bd79abaaacd3e678fac03d4c11380effac9594a7
John Mifsud <imabuddha@gmail.com>
2010-01-24 21:36:33 -0500

    Changed importer postinstall script to use the reliable method from forceImport

--- SpotlightImporter/postinstall.sh
+++ SpotlightImporter/postinstall.sh
@@ -2,5 +2,9 @@
 
 # SGF-Tools Spotlight Importer postinstall.sh
 
-echo telling mdimport to reindex all SGF files
-su ${USER} -c "/usr/bin/mdimport -r '/Library/Spotlight/SGF.mdimporter'"
+echo using find and xargs to tell mdimport to reindex all SGF files
+su ${USER} -c "find / -iname *.sgf -print0 2>/dev/null | xargs -0 mdimport &"
+
+# We were using the following Apple-recommended command, however it doesn't
+# work properly the first time SGF-Tools is installed on a machine. :/
+# su ${USER} -c "/usr/bin/mdimport -r '/Library/Spotlight/SGF.mdimporter'"
